0d147b40d05a4f5fefbd33178c570a257d70a44e
Fisto Riza Satianto <fistoriza@me.com>
2017-01-04 16:40:21 +0000

    Delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,7 +19,19 @@ class PlacesController < ApplicationController
   end
 
   def edit
-    @places = Place.find(params[:id])
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
   end
 
   private
